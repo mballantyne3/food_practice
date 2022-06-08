83b80cadfa97a4e68a9a9a98b1352ac28cef86d3
Mary Ballantyne <mballantyne777@gmail.com>
2022-06-08 11:54:35 -0400

    arrays.rb

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,7 @@
 grocery_stores = ["Kroeger", "Publix", "Albertsons", "Safeway"]
 numbers = [5, 6, 12, 24]
 floats = [0.1, 12.3, 14.6, 0.06]
-boolean = [true, true, false, false]
+booleans = [true, true, false, false]
 
 # TODO: pop
 puts "Result of pop:"
@@ -15,4 +15,8 @@ puts "Results of shift:"
 puts floats.shift
 # TODO: unshift
 puts "Results of length:"
-puts boolean.length
+puts booleans.length
+# TODO: this will show the first grocery store in array
+puts grocery_stores[0]
+# TODO: ability to add multiple types of objects in array
+puts practice = ["Kreoger", 14, 12.6]
