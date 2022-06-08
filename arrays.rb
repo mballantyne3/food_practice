9417dfbb247ffd7b7c30aeeaf2c9ff92a93535c0
Mary Ballantyne <mballantyne777@gmail.com>
2022-06-08 11:21:03 -0400

    arrays.rb

---
+++ arrays.rb
@@ -0,0 +1,7 @@
+grocery_stores = ["Kroeger", "Publix", "Albertsons", "Safeway"]
+numbers = [5, 6, 12, 24]
+floats = [0.1, 12.3, 14.6, 0.06]
+hungry = true
+cabinets_stocked = true
+gluten_free = true
+dairy_free = false
