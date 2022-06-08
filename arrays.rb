c2df1ce4bd3f0c4f50b368f2ba5750696cd22302
Mary Ballantyne <mballantyne777@gmail.com>
2022-06-08 11:43:10 -0400

    arrays.rb

--- arrays.rb
+++ arrays.rb
@@ -1,7 +1,18 @@
 grocery_stores = ["Kroeger", "Publix", "Albertsons", "Safeway"]
 numbers = [5, 6, 12, 24]
 floats = [0.1, 12.3, 14.6, 0.06]
-hungry = true
-cabinets_stocked = true
-gluten_free = true
-dairy_free = false
+boolean = [true, true, false, false]
+
+# TODO: pop
+puts "Result of pop:"
+puts grocery_stores.pop
+
+# TODO: push
+puts "Result of push:"
+puts numbers.push
+# TODO: shift
+puts "Results of shift:"
+puts floats.shift
+# TODO: unshift
+puts "Results of length:"
+puts boolean.length
